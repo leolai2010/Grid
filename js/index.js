aa4186544757740817808cd0e5ba6bec01032372
Leo Lai <38085012+leolai2010@users.noreply.github.com>
2022-07-13 08:52:07 -0700

    using jquery

--- js/index.js
+++ js/index.js
@@ -1,13 +1,35 @@
 $(document).ready(function(){
     plateSettingConstruct();
-    controlDrag();
-    const wells = document.querySelectorAll('.well');
-    wells.forEach(well => {
-        well.addEventListener('dragenter', dragEnter);
-        well.addEventListener('dragover', dragOver);
-        well.addEventListener('dragleave', dragLeave);
-        well.addEventListener('drop', drop);
+    // controlDrag();
+    // const wells = document.querySelectorAll('.well');
+    // wells.forEach(well => {
+    //     well.addEventListener('dragenter', dragEnter);
+    //     well.addEventListener('dragover', dragOver);
+    //     well.addEventListener('dragleave', dragLeave);
+    //     well.addEventListener('drop', drop);
+    // });
+    $("#plate").selectable({
+        cancel:'.handle',
+        fliter:'div'
     });
+    $("#plate").sortable({
+        handle: '.handle',
+        helper: function (e, item) {
+            if (!item.hasClass('ui-selected')) {
+                $('#plate').find('.ui-selected').removeClass('ui-selected');
+                item.addClass('ui-selected');
+            };
+            var selected = $('.ui-selected').clone();
+            item.data('multidrag', selected);
+            $('.ui-selected').not(item).remove();
+            return $('<li class="transporter" />').append(selected);
+        },
+        stop: function (e, ui) {
+            var selected = ui.item.data('multidrag');
+            ui.item.after(selected);
+            ui.item.remove();
+        }
+    }).disableSelection();
 });
 
 
@@ -20,54 +42,54 @@ function plateSettingConstruct(){
             rowCount++;
             columnCount = 1;
         };
-        var $well = $('<div/>').attr('id', plateRows[rowCount] + columnCount).addClass('well');
+        var $well = $('<div/>').attr('id', plateRows[rowCount] + columnCount).addClass('well').html(plateRows[rowCount]  + columnCount);
         $('#plate').append($well);
         columnCount++;
     };
 }
 
 /* draggable element */
-function controlDrag(){
-    const controls= document.querySelectorAll('.control');
-    controls.forEach(control=>{
-        control.addEventListener('dragstart', dragStart);
-        function dragStart(e) {
-            e.dataTransfer.setData('text/plain', e.target.id);
-            setTimeout(() => {
-                e.target.classList.add('hide');
-            }, 0);
-        }
-    })
-}
+// function controlDrag(){
+//     const controls= document.querySelectorAll('.control');
+//     controls.forEach(control=>{
+//         control.addEventListener('dragstart', dragStart);
+//         function dragStart(e) {
+//             e.dataTransfer.setData('text/plain', e.target.id);
+//             setTimeout(() => {
+//                 e.target.classList.add('hide');
+//             }, 0);
+//         }
+//     })
+// }
 
-function dragEnter(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
-}
+// function dragEnter(e) {
+//     e.preventDefault();
+//     e.target.classList.add('drag-over');
+// }
 
-function dragOver(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
-}
+// function dragOver(e) {
+//     e.preventDefault();
+//     e.target.classList.add('drag-over');
+// }
 
-function dragLeave(e) {
-    e.target.classList.remove('drag-over');
-}
+// function dragLeave(e) {
+//     e.target.classList.remove('drag-over');
+// }
 
-function drop(e) {
-    e.target.classList.remove('drag-over');
+// function drop(e) {
+//     e.target.classList.remove('drag-over');
 
-    // get the draggable element
-    var id = e.dataTransfer.getData('text/plain');
-    console.log(id)
-    console.log(document.getElementById(id))
-    var draggable = document.getElementById(id);
-    // add it to the drop target
-    e.target.appendChild(draggable);
-    console.log(draggable)
-    // display the draggable element
-    draggable.classList.remove('hide');
-    controlDrag();
-    //$(e.target).off('dragover');
-}
+//     // get the draggable element
+//     var id = e.dataTransfer.getData('text/plain');
+//     console.log(id)
+//     console.log(document.getElementById(id))
+//     var draggable = document.getElementById(id);
+//     // add it to the drop target
+//     e.target.appendChild(draggable);
+//     console.log(draggable)
+//     // display the draggable element
+//     draggable.classList.remove('hide');
+//     controlDrag();
+//     //$(e.target).off('dragover');
+// }
 
